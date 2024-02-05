27d89ea4b18734e2babdd177d119c76704eb3abc
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2024-02-05 19:46:54 +0100

    saving image helper

--- helper/image_helper.py
+++ helper/image_helper.py
@@ -9,9 +9,15 @@ def cut_in_n_times_n(image: Image.Image, n: int = 3) -> None:
     """
     assert type(n) is int, "Argument n must be an integer"
     
-    if image.width % 3 != 0 or image.height % 3 != 0:
-        image.resize((image.width - image.width % 3, image.height - image.height % 3))
+    if image.width % n != 0 or image.height % n != 0:
+        image.resize((image.width - image.width % n, image.height - image.height % n))
     
-    for y in range(image.height // 3):
-        for x in range(image.width // 3):
-            ...
+    count: int = 0
+    for y in range(0, image.height // n, n):
+        for x in range(0, image.width // n, n):
+            temp_pattern = Image.new("RGBA", (n, n))
+            for i in range(n):
+                for j in range(n):
+                    temp_pattern.putpixel((j, i), (image.getpixel((x+j, y+i))))
+            temp_pattern.save(f"image/temp/temp{count}.png")
+            count += 1
--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import tkinter
 import os
 
+from PIL import Image
+
+from helper import image_helper as ih
+
 
 if not os.path.exists("image/temp"):
     os.mkdir("image/temp")
+    
+ih.cut_in_n_times_n(Image.open("image/capture.png"))
