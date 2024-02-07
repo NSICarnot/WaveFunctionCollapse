83a240e48efd978f574d5bddd6d87f37a9b86330
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2024-02-07 16:22:02 +0100

    saving files

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ import os
 from PIL import Image
 
 from helper import image_helper as ih
-
+    
+if not os.path.exists("image/temp"):
+    os.mkdir("image/temp")
+    
 temp_files = [name for name in os.listdir("image/temp")]
 for f in temp_files:
     os.remove(f"image/temp/{f}")
 del temp_files
-    
-if not os.path.exists("image/temp"):
-    os.mkdir("image/temp")
     
 ih.cut_in_n_times_n(Image.open("image/capture.png"))
