7c934e7e354310bd6d73af74bf63857578e9d511
SorenDeveloppement <soren.evrard.lorrain@gmail.com>
2024-02-07 16:20:43 +0100

    saving files

--- helper/image_helper.py
+++ helper/image_helper.py
@@ -2,6 +2,19 @@ import os
 
 from PIL import Image
 
+# TODO: Change stuff because it's a 180° rotation
+def reflect_image(image: Image.Image) -> Image.Image:
+    width: int = image.width
+    height: int = image.height
+    
+    new_image = Image.new("RGBA", (width, height))
+    
+    for y in range(height):
+        for x in range(width):
+            new_image.putpixel((width - x - 1, height - y - 1), image.getpixel((x, y)))
+    
+    return new_image
+
 
 def cut_in_n_times_n(image: Image.Image, n: int = 3) -> None:
     """
@@ -21,3 +34,11 @@ def cut_in_n_times_n(image: Image.Image, n: int = 3) -> None:
                     temp_pattern.putpixel((j, i), (image.getpixel((x+j, y+i))))
             temp_pattern.save(f"image/temp/temp{count}.png")
             count += 1
+            
+            # reflect_image(temp_pattern).save(f"image/temp/temp{count}.png")
+            # count += 1
+            
+            for i in range(1, 4):
+                temp_pattern = temp_pattern.rotate(90 * i)
+                temp_pattern.save(f"image/temp/temp{count}.png")
+                count += 1
--- main.py
+++ main.py
@@ -5,7 +5,11 @@ from PIL import Image
 
 from helper import image_helper as ih
 
-
+temp_files = [name for name in os.listdir("image/temp")]
+for f in temp_files:
+    os.remove(f"image/temp/{f}")
+del temp_files
+    
 if not os.path.exists("image/temp"):
     os.mkdir("image/temp")
     
